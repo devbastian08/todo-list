4bc5e9008c3b663785cdc402e49b345d3c2a6769
devbastian08 <119069676+devbastian08@users.noreply.github.com>
2023-03-30 23:14:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector("[data-form-btn]");
+
+const createTask =  (evento) => {
+  evento.preventDefault();// evitar que recargue la pagina
+  const input = document.querySelector("[data-form-input]");
+  const value = input.value
+  const list = document.querySelector('[data-list]');
+  const task = document.createElement('li');
+  task.classList.add("card");
+  input.value = "";// se le da el valor al input de string vacio 
+  const content =`<div>
+          ${checkComplete}
+            <span class="task">${value}</span>
+            </div>
+            <i class="fas fa-trash-alt trashIcon icon"></i>`
+  task.innerHTML = content;
+
+  list.appendChild(task);
+  
+  console.log(content);
+};
+
+console.log(btn);
+
+btn.addEventListener("click", createTask);
+
+const checkComplete = () =>{
+  const i = document.createElement("i");
+  i.classList.add("far fa-check-square icon");
+  return i
+}
+
